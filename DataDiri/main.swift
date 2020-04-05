fa649954b1939707629230b32228db40d8c13451
Anggie <djavasoegar07@gmail.com>
2020-04-05 10:37:34 +0700

    Initial Commit

---
+++ DataDiri/main.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  DataDiri
+//
+//  Created by Bernadietta Anggie on 05/04/20.
+//  Copyright © 2020 Bernadietta Anggie. All rights reserved.
+//
+
+import Foundation
+
+
+//personal data form
+print("Welcome to Dicoding Academy")
+
+//personal data with static information
+let firstName = "Bernadietta", lastName="Anggie"
+let fullName = firstName + " " + lastName
+let address = "Jakarta"
+let job = "Mobile Developer"
+let age = 0b10001
+
+print("----------------------------------")
+//this is the output of this program
+print("Do you know \(fullName)?")
+print("\(firstName) is a \(job)")
+print("she is currently \(age) and resides in \(address)")
+print("----------------------------------")
